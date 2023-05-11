c33e5a50de57fa22fdba189106809a3e8d2c25c9
Jose Avilez <jose.avilez@partnerhero.com>
2023-05-11 12:44:58 -0600

    setup: added initial setup for graphql and added env variables extension

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
